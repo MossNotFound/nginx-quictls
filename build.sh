5110efe352adc806a6e1999d732c21824764c7ac
MossNotFound <110248437+MossNotFound@users.noreply.github.com>
2023-05-13 08:08:17 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -26,7 +26,7 @@ auto/configure --prefix=/etc/nginx --sbin-path=/usr/sbin/nginx \
 --http-proxy-temp-path=/var/cache/nginx/proxy_temp \
 --http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp \
 --http-uwsgi-temp-path=/var/cache/nginx/uwsgi_temp \
---user=www-data --group=www-data \
+--user=www-data --group=www-data --with-stream_quic_module \
 --with-file-aio --with-threads --with-pcre-jit --with-http_sub_module \
 --with-http_ssl_module --with-http_v2_module --with-http_v3_module \
 --without-select_module --without-poll_module \
